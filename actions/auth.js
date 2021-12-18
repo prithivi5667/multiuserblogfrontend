709ad7df8f25e1e6014ea98450164403a7b31f34
prithivi5667 <prithivi5667@gmail.com>
2021-12-18 16:55:42 +0530

    Fixed Configuratiion

--- actions/auth.js
+++ actions/auth.js
@@ -108,7 +108,8 @@ export const resetPassword = (resetInfo) => {
     .catch((err) => console.log(err));
 };
 export const loginWithGoogle = (user) => {
-  return fetch("http://localhost:8000/api/google-login", {
+  console.log(API)
+  return fetch(`${API}/google-login`, {
     method: 'POST',
     headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
     body: JSON.stringify(user),
--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
-
 export const isProduction = publicRuntimeConfig.PRODUCTION;
 export const API = publicRuntimeConfig.PRODUCTION
   ? publicRuntimeConfig.API_PRODUCTION
   : publicRuntimeConfig.API_DEVELOPMENT;
+console.log(API)
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
 export const DOMAIN = publicRuntimeConfig.PRODUCTION
