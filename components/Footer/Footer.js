57c754a70c05e17081a94817b045cee839a2c08e
prithivi5667 <prithivi5667@gmail.com>
2021-12-28 11:44:56 +0530

    footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
   <footer className="footer">
     <div className="footer__copyright">
-      <p>Copyright © 2020 BlogNet.com</p>
+      <p>Copyright</p>
       <p className="footer__text-and-author">
        footer
       </p>
