89292109f095f8bf9eb1ef2337ac12d317660ff6
prithivi5667 <prithivi5667@gmail.com>
2021-12-28 11:20:59 +0530

    config

--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@ const withCSS = require('@zeit/next-css')
 module.exports = {
     publicRuntimeConfig: {
         APP_NAME: 'SEOBLOG',
-        API_DEVELOPMENT: 'http://localhost:8000/api',
+        API_DEVELOPMENT: 'https://blogbackendpt.herokuapp.com/api',
         API_PRODUCTION: 'https://blogbackendpt.herokuapp.com/api',
-        PRODUCTION: false,
-        DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+        PRODUCTION: true,
+        DOMAIN_DEVELOPMENT: 'https://unruffled-mestorf-354238.netlify.app',
         DOMAIN_PRODUCTION: 'https://unruffled-mestorf-354238.netlify.app',
         FB_APP_ID: 1344095629356238,
         DISQUS_SHORTNAME: 'karmegam-in',
         GOOGLE_CLIENT_ID: '1038422740198-93tppd5fa15pridlk0445usaldanppb7.apps.googleusercontent.com',
     }
-};
+};  
