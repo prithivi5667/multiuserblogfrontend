913673b5939318743a7ef3bf869ea43bae96ae1a
prithivi5667 <prithivi5667@gmail.com>
2021-12-28 10:58:35 +0530

    new

--- config.js
+++ config.js
@@ -1,16 +1,18 @@
-import getConfig from 'next/config';
+import getConfig from "next/config";
+
 const { publicRuntimeConfig } = getConfig();
-export const isProduction = publicRuntimeConfig.PRODUCTION;
+
 export const API = publicRuntimeConfig.PRODUCTION
   ? publicRuntimeConfig.API_PRODUCTION
   : publicRuntimeConfig.API_DEVELOPMENT;
-console.log(API)
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
 export const DOMAIN = publicRuntimeConfig.PRODUCTION
   ? publicRuntimeConfig.DOMAIN_PRODUCTION
-  : publicRuntimeConfig.DOMAIN_DEVELOPMENT;
+  : publicRuntimeConfig.DOMAIN_DEVLOPMENT;
 
 export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
+
 export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
+
 export const GOOGLE_CLIENT_ID = publicRuntimeConfig.GOOGLE_CLIENT_ID;
