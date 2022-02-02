4961317a955d72fd1a3ce3e5d3aced3782acc137
prithivi5667 <77000089+prithivi5667@users.noreply.github.com>
2022-02-02 09:38:23 +0530

    Update Hero.js

--- components/HeroComponent/Hero/Hero.js
+++ components/HeroComponent/Hero/Hero.js
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-   section className="hero hero-container">
+   <section className="hero hero-container">
       <div className="slider-bg" style="background-image: url(https://www.pexels.com/@fwstudio-33348/)">
       <h2 className="hero__title">Get smarter about what you read.</h2>
       </div>
